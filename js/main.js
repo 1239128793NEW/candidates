018cced50aa9131aa347633e5bbd8d3a30a7d631
Artem Krynkin <pagesppl@gmail.com>
2018-03-02 20:52:48 +0300

    foix

--- js/main.js
+++ js/main.js
@@ -1,147 +1,101 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    var candidates = [{
+        name: 'Биг Рашн Босс',
+        vote: 0
+    }, {
+        name: 'Юрий Хованский',
+        vote: 0
+    }, {
+        name: 'Совергон',
+        vote: 0
+    }, {
+        name: 'Данил Кашин',
+        vote: 0
+    }, {
+        name: 'Lizzka',
+        vote: 0
+    }, {
+        name: 'Юлик',
+        vote: 0
+    }, {
+        name: 'Кузьма',
+        vote: 0
+    }, {
+        name: 'Даня Комков',
+        vote: 0
+    }, {
+        name: 'Маш Милаш',
+        vote: 0
+    }, {
+        name: 'Ёрник и Косс',
+        vote: 0
+    }];
 
+    $('.questions__blocList .questions__item').bind('click', function () {
+        var self = $(this);
 
-	$( "#questions__blocList_0 .questions__item" ).click(function() {
-	   	setTimeout(function() {
-	      // $( "#questions__blocList_0" ).fadeOut( 500 );
-	      // $( "#questions__blocList_1" ).fadeIn( 500 );
-	      $( ".questions__item" ).removeClass("active");
-	      $( "#questions__blocList_0" ).removeClass("show");
-	      $( "#questions__blocList_1" ).addClass("show");
-	    }, 500);
+        setTimeout(function () {
+            var currentBlockHide = self.closest('.questions__blocList');
+
+            $('.questions__item').removeClass('active');
+            currentBlockHide.removeClass('show');
+
+            candidates[self.attr('number-candidate')].vote++;
+
+            if (currentBlockHide.attr('id') !== 'questions__blocList_13') {
+                currentBlockHide.next().addClass('show');
+            } else {
+                var highestNumberVotes = [],
+                    resultIndex = 0;
+
+                candidates.filter(function (data) {
+                   return highestNumberVotes.push(data.vote);
+                });
+
+                highestNumberVotes.forEach(function (value, index) {
+                    if (Math.max.apply(Math, highestNumberVotes) === value) {
+                        resultIndex = index;
+                    }
+                });
+
+                //console.log(candidates[resultIndex]);
+                //console.log(resultIndex);
+
+                $("#end").addClass("show");
+                $('.testResultItem:visible').hide();
+                $('#page' + resultIndex).show();
+                $(".h3__test").hide(250);
+                $(".h3__resul").show(400);
+                $(".h3__resul").addClass("active");
+            }
+        }, 500);
     });
 
-    $( "#questions__blocList_1 .questions__item" ).click(function() {
-	   	setTimeout(function() {
-	      $( ".questions__item" ).removeClass("active");
-	      $( "#questions__blocList_1" ).removeClass("show");
-	      $( "#questions__blocList_2" ).addClass("show");
-	    }, 500);
+    $("#startText").click(function () {
+        setTimeout(function () {
+            $("#questions__blocList_0").addClass("show");
+            $("#end").removeClass("show");
+            $(".h3__test").show(400);
+            $(".h3__resul").hide(250);
+            $(".h3__test").addClass("active");
+
+            candidates.forEach(function (data) {
+                data.vote = 0;
+            });
+        }, 500);
     });
 
-
-    $( "#questions__blocList_2 .questions__item" ).click(function() {
-	   	setTimeout(function() {
-	      $( ".questions__item" ).removeClass("active");
-	      $( "#questions__blocList_2" ).removeClass("show");
-	      $( "#questions__blocList_3" ).addClass("show");
-	    }, 500);
+    $('.questions__item:nth-of-type(2)').click(function () {
+        $('.questions__item:nth-of-type(2)').addClass('active');
     });
-
-    $( "#questions__blocList_3 .questions__item" ).click(function() {
-	   	setTimeout(function() {
-	      $( ".questions__item" ).removeClass("active");
-	      $( "#questions__blocList_3" ).removeClass("show");
-	      $( "#questions__blocList_4" ).addClass("show");
-	    }, 500);
+    $('.questions__item:nth-of-type(3)').click(function () {
+        $('.questions__item:nth-of-type(3)').addClass('active');
     });
-
-    $( "#questions__blocList_4 .questions__item" ).click(function() {
-	   	setTimeout(function() {
-	      $( ".questions__item" ).removeClass("active");
-	      $( "#questions__blocList_4" ).removeClass("show");
-	      $( "#questions__blocList_5" ).addClass("show");
-	    }, 500);
+    $('.questions__item:nth-of-type(4)').click(function () {
+        $('.questions__item:nth-of-type(4)').addClass('active');
     });
-
-    $( "#questions__blocList_5 .questions__item" ).click(function() {
-	   	setTimeout(function() {
-	      $( ".questions__item" ).removeClass("active");
-	      $( "#questions__blocList_5" ).removeClass("show");
-	      $( "#questions__blocList_6" ).addClass("show");
-	    }, 500);
+    $('.questions__item:nth-of-type(5)').click(function () {
+        $('.questions__item:nth-of-type(5)').addClass('active');
     });
-
-    $( "#questions__blocList_6 .questions__item" ).click(function() {
-	   	setTimeout(function() {
-	      $( ".questions__item" ).removeClass("active");
-	      $( "#questions__blocList_6" ).removeClass("show");
-	      $( "#questions__blocList_7" ).addClass("show");
-	    }, 500);
-    });
-
-    $( "#questions__blocList_7 .questions__item" ).click(function() {
-	   	setTimeout(function() {
-	      $( ".questions__item" ).removeClass("active");
-	      $( "#questions__blocList_7" ).removeClass("show");
-	      $( "#questions__blocList_8" ).addClass("show");
-	    }, 500);
-    });
-
-    $( "#questions__blocList_8 .questions__item" ).click(function() {
-	   	setTimeout(function() {
-	      $( ".questions__item" ).removeClass("active");
-	      $( "#questions__blocList_8" ).removeClass("show");
-	      $( "#questions__blocList_9" ).addClass("show");
-	    }, 500);
-    });
-
-    $( "#questions__blocList_9 .questions__item" ).click(function() {
-	   	setTimeout(function() {
-	      $( ".questions__item" ).removeClass("active");
-	      $( "#questions__blocList_9" ).removeClass("show");
-	      $( "#questions__blocList_10" ).addClass("show");
-	    }, 500);
-    });
-
-    $( "#questions__blocList_10 .questions__item" ).click(function() {
-	   	setTimeout(function() {
-	      $( ".questions__item" ).removeClass("active");
-	      $( "#questions__blocList_10" ).removeClass("show");
-	      $( "#questions__blocList_11" ).addClass("show");
-	    }, 500);
-    });
-
-    $( "#questions__blocList_11 .questions__item" ).click(function() {
-	   	setTimeout(function() {
-	      $( ".questions__item" ).removeClass("active");
-	      $( "#questions__blocList_11" ).removeClass("show");
-	      $( "#questions__blocList_12" ).addClass("show");
-	    }, 500);
-    });
-
-    $( "#questions__blocList_12 .questions__item" ).click(function() {
-	   	setTimeout(function() {
-	      $( ".questions__item" ).removeClass("active");
-	      $( "#questions__blocList_12" ).removeClass("show");
-	      $( "#questions__blocList_13" ).addClass("show");
-	    }, 500);
-    });
-
-    $( "#questions__blocList_13 .questions__item" ).click(function() {
-	   	setTimeout(function() {
-	      $( ".questions__item" ).removeClass("active");
-	      $( "#questions__blocList_13" ).removeClass("show");
-	      $( "#end" ).addClass("show");
-	      $('.testResultItem:visible').hide().siblings().eq(Math.floor(Math.random() * 9)).show();
-	      $( ".h3__test" ).hide( 250 );
-	      $( ".h3__resul" ).show( 400 );
-	      $( ".h3__resul" ).addClass("active");
-	    }, 500);
-    });
-
-    $( "#startText" ).click(function() {
-	   	setTimeout(function() {
-	      $( "#questions__blocList_0" ).addClass("show");
-	      $( "#end" ).removeClass("show");
-	      $( ".h3__test" ).show( 400 );
-	      $( ".h3__resul" ).hide( 250 );
-	      $( ".h3__test" ).addClass("active");
-	    }, 500);
-    });
-
-
-    $('.questions__item:nth-of-type(2)').click(function(){
-	  $('.questions__item:nth-of-type(2)').addClass('active');
-	});
-	$('.questions__item:nth-of-type(3)').click(function(){
-	  $('.questions__item:nth-of-type(3)').addClass('active');
-	});
-	$('.questions__item:nth-of-type(4)').click(function(){
-	  $('.questions__item:nth-of-type(4)').addClass('active');
-	});
-	$('.questions__item:nth-of-type(5)').click(function(){
-	  $('.questions__item:nth-of-type(5)').addClass('active');
-	});
 
 });
